5f05c781152d6a80f2e352c55ab6283e55560cd5
Alex Teerlinck <ateerlinckwork@gmail.com>
2024-02-20 23:01:37 -0600

    radix sort fixed

--- PA1/JAVA/RadixSort.java
+++ PA1/JAVA/RadixSort.java
@@ -29,18 +29,21 @@ public class RadixSort {
 
 	public static void radixSort(int[] array, int n) { // complete this function
 		//Approach 2 will be awarded 5 bonus points. Avoiding the creation of extra arrays for the negative and non-negative integers in Approach 2 will be rewarded 10 bonus points.
+		int[] NEG = new int[n], NONNEG = new int[n];
 		int negativeCount = 0;
 		for (int i = 0; i < array.length; i++) {
-			if(array[i]<0){
-				array[i]+=555555;
+			if(array[i]<0) {
+				NEG[i] = Math.abs(array[i]);
 				negativeCount++;
 			}
+			else NONNEG[i] = array[i];
 		}
-		radixSortNonNeg(array, n);
-		for (int i = 0; i < negativeCount; i++) {
-			array[n-negativeCount+i]-=555555;
-			Partition.swap(array, n-negativeCount+i, i);
-			for (int j = negativeCount; j < array.length-1; j++) Partition.swap(array, n-j+i, n-j+i-1);
+		radixSortNonNeg(NEG, n);
+		radixSortNonNeg(NONNEG, n);
+		for (int i = 0; i < n; i++) {
+			if(i<negativeCount) array[i] = -NEG[n-i-1];
+			else array[i] = NONNEG[i];
 		}
+		negativeCount++;
 	}
 }
